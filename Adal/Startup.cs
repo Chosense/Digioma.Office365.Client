c978a580d330516d2a15db0249cd061bc06aca28
Mika Berglund <mika.berglund@develore.com>
2016-01-28 17:57:39 +0200

    Added a class that takes care of authenticaton configuration for OWIN

---
+++ Adal/Startup.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Owin;
+using System;
+using System.IdentityModel.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Owin;
+
+
+
+namespace Digioma.Office365.Client.Adal
+{
+    public static class Startup
+    {
+        public static void ConfigureAuth(IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+            app.UseOpenIdConnectAuthentication(
+                new OpenIdConnectAuthenticationOptions
+                {
+                    ClientId = AppSettings.ClientId,
+                    Authority = AppSettings.Authority,
+
+                    Notifications = new OpenIdConnectAuthenticationNotifications()
+                    {
+                    // If there is a code in the OpenID Connect response, redeem it for an access token and refresh token, and store those away.
+                    AuthorizationCodeReceived = (context) =>
+                        {
+                            var code = context.Code;
+                            ClientCredential credential = new ClientCredential(AppSettings.ClientId, AppSettings.AppKey);
+                            String signInUserId = context.AuthenticationTicket.Identity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+                            AuthenticationContext authContext = new AuthenticationContext(AppSettings.Authority, new ADALTokenCache(signInUserId));
+                            AuthenticationResult result = authContext.AcquireTokenByAuthorizationCode(code, new Uri(HttpContext.Current.Request.Url.GetLeftPart(UriPartial.Path)), credential, AppSettings.AADGraphResourceId);
+
+                            return Task.FromResult(0);
+                        },
+                        RedirectToIdentityProvider = (context) =>
+                        {
+                        // This ensures that the address used for sign in and sign out is picked up dynamically from the request
+                        // this allows you to deploy your app (to Azure Web Sites, for example)without having to change settings
+                        // Remember that the base URL of the address used here must be provisioned in Azure AD beforehand.
+                        string appBaseUrl = context.Request.Scheme + "://" + context.Request.Host + context.Request.PathBase;
+                            context.ProtocolMessage.RedirectUri = appBaseUrl + "/";
+                            context.ProtocolMessage.PostLogoutRedirectUri = appBaseUrl;
+
+                            return Task.FromResult(0);
+                        },
+                        AuthenticationFailed = (context) =>
+                        {
+                        // Suppress the exception if you don't want to see the error
+                        context.HandleResponse();
+                            return Task.FromResult(0);
+                        }
+                    }
+
+                });
+        }
+
+    }
+}
